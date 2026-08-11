5457be99844bfa92133a30d9f9b067233b2021ad
Pratyaksh Gupta <pratyaksh.37999@stu.upes.ac.in>
2026-08-11 19:48:36 +0530

    Day 13 - Q025, Q026

---
+++ days/Day13/Q025.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    char sign;
+    printf("Enter nos. and operator: ");
+    scanf("%d %d %c", &a,&b,&sign);
+    switch (sign){
+        case '+':
+            printf("%d",a+b);
+            break;
+        case '-':
+            printf("%d",a-b);
+            break;
+        case '*':
+            printf("%d",a*b);
+            break;
+        case '/':
+            printf("%d",a/b);
+            break;
+        case '%':
+            printf("%d",a%b);
+            break;
+    }
+    return 0;
+
+}
---
+++ days/Day13/Q026.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter no. of terms: ");
+    scanf("%d", &n);
+    for (int j=1; j<=n; j++){
+        printf("%d ",j);
+    }
+    return 0;
+
+}
