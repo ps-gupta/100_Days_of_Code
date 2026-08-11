aabc8df0427ac24596a741844e6fa8eff42bc3b1
Pratyaksh Gupta <pratyaksh.37999@stu.upes.ac.in>
2026-08-11 19:48:36 +0530

    Day 16 - Q031

---
+++ days/Day16/Q031.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i=0;
+    int binary[32];
+
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    while (n>0){
+        binary[i++] = n & 1;
+        n>>=1;
+    }
+
+    while (i>0){
+        printf("%d", binary[--i]);
+    }
+    return 0;
+
+}
