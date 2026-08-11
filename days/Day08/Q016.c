fd5f4c2e86f33c361d1b3b9e64e194b8e7814153
Pratyaksh Gupta <pratyaksh.37999@stu.upes.ac.in>
2026-08-11 19:48:35 +0530

    Day 08 - Q016

---
+++ days/Day08/Q016.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1,num2,num3;
+    printf("Enter numbers to check: ");
+    scanf("%d %d %d", &num1,&num2,&num3);
+    if (num1>num2 && num1>num3){
+        printf("Largest is %d",num1);
+    }
+    else if (num2>num1 && num2>num3){
+        printf("Largest is %d",num2);
+    }
+    else{
+        printf("Largest is %d",num3);
+    }
+    return 0;
+
+}
