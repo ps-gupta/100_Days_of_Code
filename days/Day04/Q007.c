d10097bf759a757ab59036264e404afc8a2e7a9b
Pratyaksh Gupta <pratyaksh.37999@stu.upes.ac.in>
2026-08-11 19:48:35 +0530

    Day 04 - Q007, Q008

---
+++ days/Day04/Q007.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Enter 2 Nos.: ");
+    scanf("%d %d", &a, &b);
+    a= a+b;
+    b = a-b;
+    a = a-b;
+
+    printf("After swap: %d %d",a,b);
+    return 0;
+
+}
