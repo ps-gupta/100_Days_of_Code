3106f407fe1a34be847fbc91525746c0f56ed6e8
Pratyaksh Gupta <pratyaksh.37999@stu.upes.ac.in>
2026-08-11 19:48:36 +0530

    Day 15 - Q029

---
+++ days/Day15/Q029.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    start_of_code:
+    int n;
+    unsigned long long fact=1;
+    printf("Enter a positive number: ");
+    scanf("%d", &n);
+    if (n<0){
+        printf("!!!Error: Enter a positive number!!!");
+        goto start_of_code;
+    }
+    else{
+        for (int i=1; i<=n;i++){
+            fact = fact*i;
+        }
+        printf("%llu",fact);
+    }
+    return 0;
+
+}
