d937400f4bd5f87f2aea888bad62e16b5345c0d2
Pratyaksh Gupta <pratyaksh.37999@stu.upes.ac.in>
2026-08-11 19:48:35 +0530

    Day 03 - Q005, Q006

---
+++ days/Day03/Q005.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int Celcius,Fahrenheit;
+    printf("Enter temp: ");
+    scanf("%d",&Celcius);
+    Fahrenheit = (9.0/5.0*Celcius)+32;
+    printf("Fahrenheit=%d",Fahrenheit);
+
+    return 0;
+
+}
---
+++ days/Day03/Q006.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    c=0;
+    printf("Enter 2 Nos.: ");
+    scanf("%d %d",&a,&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("After swap: %d %d",a,b);
+
+
+    return 0;
+
+}
