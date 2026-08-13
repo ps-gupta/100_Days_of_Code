ad2c78c7924a0e3853717dd36fdaa58e0dc4097e
Pratyaksh Gupta <Pratyaksh.37999@stu.upes.ac.in>
2026-08-13 12:27:37 +0530

    Change fine output format to include '₹' symbol
    Updated fine calculation messages to include currency symbol.

--- days/Day12/Q023.c
+++ days/Day12/Q023.c
@@ -1,7 +1,7 @@
 /* Write a program to calculate library fine based on late day as follows:
-First 5 days late: 
-Next 5 days late:
-Next 20 days late:
+First 5 days late: ₹2/day
+Next 5 days late: ₹4/day
+Next 20 days late: ₹6/day
 More than 30 days: Membership Cancelled */
 
 #include <stdio.h>
@@ -13,15 +13,15 @@ int main() {
     if (days<=30 && days>0){
         if (days <=5){
             fine = days*2;
-            printf("Fine %d",fine);
+            printf("Fine ₹%d",fine);
         }
         else if (days > 5 && days <= 10){
             fine = 10 + (4*(days-5));
-            printf("Fine %d",fine);
+            printf("Fine ₹%d",fine);
         }
         else if (days>10 && days<= 30){
             fine = 30 + (6*(days-10));
-            printf("Fine %d",fine);
+            printf("Fine ₹%d",fine);
         }
     }
     else if (days>30){
