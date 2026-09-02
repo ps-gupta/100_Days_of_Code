8ee1c15eead44cc37fd88bbb7a5a03f7c711b9fd
Pratyaksh Gupta <pratyaksh.37999@stu.upes.ac.in>
2026-09-02 13:46:25 +0530

    Day 33 - Q065

---
+++ days/Day33/Q065.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int size;
+    printf("Enter size of array: ");
+    scanf("%d",&size);
+    int arr[size];
+    printf("Enter the elememnts: ");
+    for (int i=0; i<size; i++){
+        scanf("%d", arr[i]);
+    }
+    int find;
+    printf("Enter the num to search: ");
+    scanf("%d", &find);
+    int index= 0
+    qsort (arr,size,sizeof(int),compare);
+    for (int i=0; i<size; i++){
+        if (arr[size/2]==)
+    }
+    return 0;
+
+}
