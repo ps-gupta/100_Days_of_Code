143ddb5c03da6bf64683c56491d9cc132a0cdc44
Pratyaksh Gupta <pratyaksh.37999@stu.upes.ac.in>
2026-09-02 22:14:58 +0530

    Day 33 - Q065

--- days/Day33/Q065.c
+++ days/Day33/Q065.c
@@ -1,23 +1,45 @@
+// Search in a sorted array using binary search.
+
 #include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int size;
-    printf("Enter size of array: ");
+int main (void){
+    int size,num;
+    printf("Enter the size of array: ");
     scanf("%d",&size);
-    int arr[size];
-    printf("Enter the elememnts: ");
-    for (int i=0; i<size; i++){
-        scanf("%d", arr[i]);
-    }
-    int find;
-    printf("Enter the num to search: ");
-    scanf("%d", &find);
-    int index= 0
-    qsort (arr,size,sizeof(int),compare);
-    for (int i=0; i<size; i++){
-        if (arr[size/2]==)
-    }
-    return 0;
 
+    int run=size, arr[size], temp=0;
+
+    printf("Enter elements of array: ");
+    for (int i=0; i<size; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    while (run!=0){
+        for (int i=0; i<size-1; i++){
+            if (arr[i]>arr[i+1]){
+                temp = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        run--;
+    }
+    run = size-1;
+
+    printf("Enter the no. to search: ");
+    scanf("%d", &num);
+
+    do{
+        if (num<(arr[run/2+run%2])){
+            run = run/2+run%2;
+        }
+        else if (num == arr[run]){
+            goto end;
+        }
+        else{
+            run = ((run/2+size-1)/2)+((run/2+size-1)%2);
+        }
+    }while(1);
+    end:
+    printf("Found at index %d", run);
+    return 0;
 }
